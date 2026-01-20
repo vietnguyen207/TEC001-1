13ccf0a2d3606ab9b2cd1f9b45e9fae5d5deb118
Tri Viet <viettringuyen207@gmail.com>
2026-01-20 07:35:22 +0700

    Delete Sum-Product-Average.py

--- Sum-Product-Average.py
+++
@@ -1,8 +0,0 @@
-# Sum, Product and average of numbers calculation
-x = int(input("Enter the first integer: "))
-y = int(input("Enter the second integer: "))
-z = int(input("Enter the third integer: "))
-s = x+y+z
-pr = x*y*z
-avr = x+y+z/3
-print (f"The sum of 3 integers is: {s} \nThe product of 3 intergers is: {pr} \nThe averacge of 3 integers is: {avr:.2f}")
