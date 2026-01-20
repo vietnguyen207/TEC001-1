8b6c517e2227dd400e2aeab2b9e70445efa7920c
Tri Viet <viettringuyen207@gmail.com>
2026-01-20 07:35:34 +0700

    Delete Rectangle Calculation.py

--- Rectangle Calculation.py
+++
@@ -1,6 +0,0 @@
-# Rectangle Perimeter and area calculation
-l = float(input("Enter the length of rectangle (meter): "))
-w = float(input("Enter the width of rectangle (meter): "))
-p = 2*l+2*w
-a = l*w
-print (f"The perimeter of rectangle is: {p:.2f}m \nThe area of rectangle is: {a:.2f}m^2")
