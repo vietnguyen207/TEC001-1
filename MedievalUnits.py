9b63337e2c0a5ff27f28a9a8e6b2ea376232c467
Tri Viet <viettringuyen207@gmail.com>
2026-01-20 07:35:11 +0700

    Delete MedievalUnits.py

--- MedievalUnits.py
+++
@@ -1,8 +0,0 @@
-# Medieval units calculation
-t = float(input("Enter talents: "))
-pd = float(input("Enter pounds: "))
-ls = float(input("Enter lots: "))
-tpl = t*20*32*13.3 + pd*32*13.3 + ls*13.3
-kg = tpl // 1000
-gr = tpl % 1000
-print (f"The weight in modern units: {kg}Kilograms and {gr}grams")
