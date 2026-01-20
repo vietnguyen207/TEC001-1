ee5a988ab0880e32defa44ce7a7ef4187a80edb3
Tri Viet <viettringuyen207@gmail.com>
2026-01-20 07:34:59 +0700

    Delete CombinationLock.py

--- CombinationLock.py
+++
@@ -1,6 +0,0 @@
-import random
-# Combination Lock
-code_3_digit = [random.randint(0, 9) for _ in range(3)]
-code_4_digit = [random.randint(1, 6) for _ in range(4)]
-print("3-digit combination lock code (0–9):", code_3_digit)
-print("4-digit combination lock code (1–6):", code_4_digit)
